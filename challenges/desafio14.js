7de34e71f5aec79b425b7b1a4578a6d49aa7a6f1
EduMLAlmeida <edualmeidawebdev@gmail.com>
2023-01-12 15:03:33 -0300

    Atende requisitos 13 e 14,

---
+++ challenges/desafio14.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany(
+  {},
+  { $push: { valoresNutricionais: { $each: [], $slice: 3 } } },
+  );
+
+db.produtos.find(
+  { ingredientes: { $in: ["picles"] } },
+  { nome: 1, ingredientes: 1, valoresNutricionais: 1, _id: 0 },
+  );
